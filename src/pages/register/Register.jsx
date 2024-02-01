6275d3a33e412d509c7158a19209d5b47cf218b3
Tejendrasinh <tejendrasinh6844@gmail.com>
2024-02-01 08:34:56 +0530

    1feb-updated

--- src/pages/register/Register.jsx
+++ src/pages/register/Register.jsx
@@ -14,6 +14,7 @@ export default function Register() {
   const usernameRef = useRef();
   const axiosInstance = axios.create({
     baseURL:process.env.REACT_APP_API_URL,
+    headers: 'allow-access-origin'
   })
   const handleStart = () => {
     setEmail(emailRef.current.value);
